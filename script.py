6688822fa9271900012d462236b7331a50fff50d
Mateus Larrubia <mateus.larrubia@mercadolivre.com>
2019-08-16 14:08:50 -0300

    add requests + single dependency principle

--- script.py
+++ script.py
@@ -3,17 +3,26 @@ import os.path
 import requests
 import csv
 
-csv_filename = 'users.csv'
+def getUserInfo(id):
+	res = requests.get(f'https://jsonplaceholder.typicode.com/users?id={id}')
+	return res.json()[0]
 
-if not os.path.exists(csv_filename):
-	print('O arquivo nao pode ser encontrado')
-	sys.exit()
-
-with open(csv_filename) as csv_file:
-	csv_reader = csv.reader(csv_file, delimiter = ',')
+def readCsv(file):
+	csv_reader = csv.reader(file, delimiter = ',')
 	line_count = 0
 	for row in csv_reader:
 		if line_count > 0:
-			print('user_id:', row[0])
+			handleDataFromCsv(row)
 		line_count += 1
-	print('Processed', line_count, 'lines.')
+	print(f'\nProcessed {line_count - 1} lines.')
+
+def handleDataFromCsv(row):
+	user = getUserInfo(row[0])
+	print(user['name'])
+
+if __name__ == "__main__":
+	csv_filename = 'users.csv'
+	if not os.path.exists(csv_filename):
+		print('O arquivo nao pode ser encontrado')
+		sys.exit()
+	readCsv(open(csv_filename))
